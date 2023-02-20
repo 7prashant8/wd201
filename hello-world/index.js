b05c464251556a8ec1ae01756c620a7c36b75349
Abhishek Bante <abhishek.bante.cse@ghrce.raisoni.net>
2023-02-20 19:52:14 +0530

    changed file

--- hello-world/index.js
+++ hello-world/index.js
@@ -1,17 +1,1 @@
-const fs=require("fs");
-
-// fs.writeFile("sample.txt",
-//         "Hello World! I am created!",
-//         (err)=>{
-            
-//             if(err){
-//                 throw err;
-//             }
-//             else console.log('File Created!')
-//         }
-//         );
-fs.readFile('sample.txt',
-            (err,data)=>{
-                if(err) throw err;
-                else console.log(data.toString());
-            })
+console.log("Hello GitHub!");
