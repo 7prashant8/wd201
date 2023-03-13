26cdbd790a341aedb38398fd05617562c4cb8473
Abhishek Bante <abhishek.bante.cse@ghrce.raisoni.net>
2023-03-13 20:38:00 +0530

    Commit

--- todo-app/models/todos.js
+++ todo-app/models/todos.js
@@ -9,12 +9,17 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
+    // eslint-disable-next-line no-unused-vars
     static associate(models) {
       // define association here
     }
 
-    static async addTask(params) {
-      return await Todo.create(params);
+    static addTodo({ title, dueDate }) {
+      return this.create({ title: title, dueDate: dueDate, completed: false });
+    }
+
+    markAsCompleted() {
+      return this.update({ completed: true });
     }
 
   }
