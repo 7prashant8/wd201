9a9f57cae5ce365139bf4526d166cc83313d409d
Abhishek Bante <abhishek.bante.cse@ghrce.raisoni.net>
2023-04-30 17:25:37 +0530

    added backend to check password is not emmpty in sign up

--- todo-app/app.js
+++ todo-app/app.js
@@ -172,7 +172,10 @@ app.post("/users", async (request, response) => {
     request.flash("error", "Enter email");
     return response.redirect("/signup");
   }
-
+  if (!request.body.password) {
+    request.flash("error", "Enter password");
+    return response.redirect("/signup");
+  }
   //console.log(hashPwd)
   try {
     const User = await user.create({
